ecb9abd7fd624406ed194e8a8b4de087bef447d1
Debasish <debasishdas2439@gmail.com>
2025-04-26 01:45:28 +0530

    setting up, but facing error with docker

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+
+const app = express()
+dotenv.config({path:'./.env'})
+const port = process.env.PORT
+
+app.listen(port, ()=>{
+    console.log(`the server is running on port: ${port}`);
+    
+})
